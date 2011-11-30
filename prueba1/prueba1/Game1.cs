64b2fc2ab0fcb37dd4817bb22d92d6a73e0c3f62
agustinbulgach@gmail.com <agustinbulgach@gmail.com@c793985e-a147-7ee1-3326-a611f7440577>
2011-11-30 22:36:52 +0000

    scale de pantalla y andando el salto.

--- prueba1/prueba1/Game1.cs
+++ prueba1/prueba1/Game1.cs
@@ -13,7 +13,7 @@ namespace prueba1
 {
     public class Game1 : Microsoft.Xna.Framework.Game
     {
-       
+
         readonly GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
         SpriteFont font;
@@ -22,7 +22,9 @@ namespace prueba1
         public static int screenHeight;
         public static Texture2D backgroundViewTex;
         public static Texture2D backgroundCollisionTex;
-        
+        public static int scaleFactor = 2;
+
+
         private Player player;
 
         public Game1()
@@ -32,33 +34,32 @@ namespace prueba1
         }
         protected override void Initialize()
         {
-            
 
-            graphics.PreferredBackBufferWidth = 184 ;
-            graphics.PreferredBackBufferHeight = 145 ;
-            graphics.IsFullScreen = false;
+
+            graphics.PreferredBackBufferWidth = 184 * scaleFactor;
+            graphics.PreferredBackBufferHeight = 145 * scaleFactor;
+            graphics.IsFullScreen = true;
+
             graphics.ApplyChanges();
             Window.Title = "Prueba1";
 
-            
-
             player = new Player
-                         {
-                             Pos = new Vector2(20, 20),
-                             State = "Idle",
-                             GravityForce = .5f,
-                             Gravity = 0,
-                             JumpForce = -2,
-                             jumpDecr= .75f,
-                             Velocity = 0,
-                             MaxVelocity = 1,
-                             Acceleration = 1,
-                             jump = 0,
-                             jumpKeyPressed=0,
-                             Tex=new AnimatedSprite(Content.Load<Texture2D>("playeranim"), 0, 16, 32),
-                             
-            
-                         };
+            {
+                Pos = new Vector2(20, 20),
+                State = "Idle",
+                GravityForce = .5f,
+                Gravity = 0,
+                JumpForce = -2,
+                jumpDecr = .75f,
+                Velocity = 0,
+                MaxVelocity = 1,
+                Acceleration = 1,
+                jump = 0,
+                jumpKeyPressed = 0,
+                Tex = new AnimatedSprite(Content.Load<Texture2D>("playeranim"), 0, 16, 32),
+
+
+            };
 
             base.Initialize();
         }
@@ -67,20 +68,20 @@ namespace prueba1
             spriteBatch = new SpriteBatch(GraphicsDevice);
             font = Content.Load<SpriteFont>("font");
             device = graphics.GraphicsDevice;
-            
+
             backgroundViewTex = Content.Load<Texture2D>("bg1texture");
             backgroundCollisionTex = Content.Load<Texture2D>("bg1collision");
             screenWidth = device.PresentationParameters.BackBufferWidth;
             screenHeight = device.PresentationParameters.BackBufferHeight;
 
-          
+
         }
         protected override void UnloadContent()
         {// TODO: Unload any non ContentManager content here
         }
         protected override void Update(GameTime gameTime)
         {
-            
+
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
             if (Keyboard.GetState().IsKeyDown(Keys.Tab))
@@ -94,46 +95,36 @@ namespace prueba1
         }
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-            Matrix globalTransformation = Matrix.CreateScale(1, 1, 1);
+            GraphicsDevice.Clear(Color.Black);
+            Matrix globalTransformation = Matrix.CreateScale(scaleFactor, scaleFactor, 1);
+
+            globalTransformation.Translation = new Vector3((screenWidth - backgroundViewTex.Width * scaleFactor) / 2, (screenHeight - backgroundViewTex.Height * scaleFactor) / 2, 0);
             spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null, globalTransformation);
+
             DrawBackground();
             DrawPlayer();
             DrawText();
-            
+
             spriteBatch.End();
             base.Draw(gameTime);
         }
 
         private void DrawText()
         {//LO DEJO DE GUIA POR SI NECESITAMOS ESCRIBIR TEXTO :p
-            spriteBatch.DrawString(font, "State: " + player.State, new Vector2(10, (int)(screenHeight/6)), Color.Red);
+            spriteBatch.DrawString(font, "State: " + player.State, new Vector2(10, (int)(screenHeight / 6)), Color.Red);
         }
 
         private void DrawBackground()
         {
-            var screenRectangle = new Rectangle(0, 0, screenWidth, screenHeight);
-            spriteBatch.Draw(backgroundViewTex, screenRectangle, Color.White);
+            Vector2 bgpos = new Vector2(0, 0);
+            //var screenRectangle = new Rectangle(0, 0, screenWidth, screenHeight);
+            spriteBatch.Draw(backgroundViewTex, bgpos, Color.White);
         }
 
         private void DrawPlayer()
         {
+            //spriteBatch.Draw(player.Tex.Texture, player.Pos, player.Tex.SourceRect, Color.White, 0f, player.Tex.Origin, 1.0f, SpriteEffects.None, 0);
             spriteBatch.Draw(player.Tex.Texture, player.Pos, player.Tex.SourceRect, Color.White, 0f, player.Tex.Origin, 1.0f, SpriteEffects.None, 0);
-        }
-
-
-        private static bool EstaColisionando(Vector2 position, Texture2D texture, Color color, int addX, int addY)
-        {
-            var textureDataBG = new Color[(texture.Width * texture.Height)];
-            texture.GetData(textureDataBG);
-
-            if (((int)position.Y + addY) * texture.Width + ((int)position.X + addX) <= textureDataBG.Length)
-                if (textureDataBG[((int)position.Y + addY) * texture.Width + ((int)position.X + addX)] != color)
-                    return false;
-
-
-
-            return true;
         }
     }
 }
